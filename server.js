d2b4752c6b08216e41f3feeefa1f6f08c1a4d60a
victoryforphil <victoryforphil@gmail.com>
2019-02-10 21:55:15 -0800

    aDbhabhdad

--- server.js
+++ server.js
@@ -5,37 +5,42 @@ var Client = require("vicconnect-client");
 var client = new Client("vc-web-dash", "Victory Connect Web Dash");
 var tickRate = 50;
 var packets = 0;
-client.enableTCP();
 
-app.get('/', function(req, res){
+client.useMDNS((ip, port) => {
+  client.enableTCP(ip, port);
+}, () => { })
+
+client.on("ready", () => {
+  client.setTickRate(tickRate);
+  client.subscribe("*", (packet) => {
+    io.emit("topic", packet);
+    packets++;
+  })
+
+  client.registerCommand("test/web/alert", (packet) => {
+    io.emit("alert", packet.data[0]);
+  })
+})
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/web_script.js', function(req, res){
+app.get('/web_script.js', function (req, res) {
   res.sendFile(__dirname + '/web_script.js');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('a user connected');
-  
-  client.on("ready", ()=>{
-    client.setTickRate(tickRate);
-  client.subscribe("*", (packet)=>{
-      io.emit("topic",packet);
-      packets++;
-  })
 
-  client.registerCommand("test/web/alert",(packet)=>{
-    io.emit("alert", packet.data[0]);
-  })
-  })
+ 
 
-  socket.on("command_send",(packet)=>{
+  socket.on("command_send", (packet) => {
     console.log("Sending Command: ", packet);
     client.callCommand(packet.path, packet.data);
   })
 
-  setInterval(()=>{
+  setInterval(() => {
     io.emit("stats", {
       tickRate: tickRate,
       packets: packets
@@ -43,6 +48,6 @@ io.on('connection', function(socket){
   }, 1000)
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
